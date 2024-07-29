1a7611bb29cbd7670dc9a0d6ca8852120769e673
gr <wkdwlgjs111@naver.com>
2024-07-29 15:13:59 +0900

    com

--- src/main/front/src/App.js
+++ src/main/front/src/App.js
@@ -1,8 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
+
+    useEffect(() => {
+        axios.get('http://localhost:8080/api/hello')
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.log(error.data);
+            });
+    }, []); // 빈 배열은 컴포넌트가 처음 렌더링될 때 한 번만 실행됨
+
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
